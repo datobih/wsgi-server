a273dce84df3d66f19c9f2316d88473c6b6e115e
Ayodele David <dayodele89@outlook.com>
2023-05-02 23:45:20 +0100

    Added basic model operation parsing to SQL statements

--- orm.py
+++ orm.py
@@ -1,7 +1,5 @@
-from typing import Any
-
-
 class Model():
+    id=None
     def __init__(self, *args, **kwargs): 
         # Get all class attributes aside inbuilt attributes
         attrs=[i for i in dir(self) if i[0]!='_']
@@ -9,10 +7,12 @@ class Model():
             # Ignore overriden getattribute in this class
             self.__dict__[attribute]=object.__getattribute__(self,attribute)
             
+            
 
     #Return Field value instead of object reference
-    def __getattribute__(self, __name: str,**kwargs) -> Any:
+    def __getattribute__(self, __name: str,**kwargs):
         attr=object.__getattribute__(self,__name)
+        
         if(isinstance(attr,Field)):
             return attr.value
         else:
@@ -21,12 +21,17 @@ class Model():
 
     #Make sure when setting fields the type matches the field
     def __setattr__(self, __name: str, __value) -> None:
-
         if(isinstance(self.__dict__[__name],Field)):
             #Set the value of field from object attribute dictionary
             self.__dict__[__name].value=__value
-            
 
+    def all(self):
+        # Get table name using model name 
+        name=get_plural(type(self).__name__)
+        query=f'SELECT * FROM {name}'
+        print(query)
+
+            
 class Field():
     def __init__(self,**kwargs):
         
@@ -44,19 +49,30 @@ class IntField(Field):
 
 
 class Person(Model):
-    name=CharField()
-
-    
-
-    
-    
-
-    
-    
+    name=CharField(value='ds')
 
 
-    
+def create_database(model):
+    #Get table name using model name
+    name=get_plural(model.__name__)
+    query=f'''CREATE TABLE IF NOT EXISTS {name}(
+        id INTEGER PRIMARY KEY,
+        '''
+    for attribute in model.__dict__:
+        if attribute[0]!='_':
+            field=object.__getattribute__(model,attribute)
+            if(isinstance(field,CharField)):
+                query+=f'{attribute} TEXT,'
+                
+    query+=')'
+    print(query)
 
-a=Person()
-a.name="DWW"
-print(a.name)
+def get_plural(name):
+    name=name.lower()
+    if(not name.endswith('s')):
+        name+='s'
+    return name
+
+
+# create_database(Person)
+Person().all()
